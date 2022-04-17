b263fd800c8adf1813c42245ae2fabcc109e689f
Alexey Kulikov <alexeykulikov30@gmail.com>
2022-04-17 20:43:54 +0300

    feat: add simple slider to the main page on the pets section

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -24,3 +24,29 @@ function closeMenu(event) {
 navLinks.forEach((el) => el.addEventListener('click', closeMenu));
 
 /* end hamburger menu */
+
+/* slider */
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+let img = document.querySelector('.pets_slider_content')
+let images = document.querySelectorAll('.cart')
+let leftInit = 360
+let count = 3
+
+let position = 0
+
+prev.onclick = function() {
+    position += leftInit*count
+    if (position > 0) position = -(images.length - 1)*leftInit
+    img.style.left = position + 'px'
+}
+
+next.onclick = function() {
+    position -= leftInit*count
+    // position = Math.max(position, -leftInit * (images.length - count))
+    if (position <= -(images.length - 1)*leftInit) position = 0
+    img.style.left = position + 'px'
+}
+
+/* end slider */
